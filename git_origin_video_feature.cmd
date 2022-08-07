03dea6de573edeccc68ec6ff1addf0534b4ecea4
artamir <artamir@mail.ru>
2022-08-07 11:51:29 +0300

    .

--- git_origin_video_feature.cmd
+++ git_origin_video_feature.cmd
@@ -2,8 +2,8 @@ echo "# corp-video-features" >> README.md
 git init
 git add README.md
 git commit -m "first commit"
-git branch -M main
+git branch -M master
 git remote add origin https://github.com/artamir/corp-video-features.git
-git push -u origin main
+git push -u origin master
 
 pause
