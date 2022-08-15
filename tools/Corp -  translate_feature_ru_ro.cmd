74191e57d13a043370d200338e7f8319c9b6d9d0
artamir <artamir@mail.ru>
2022-08-15 12:32:18 +0300

    .

---
+++ tools/Corp -  translate_feature_ru_ro.cmd
@@ -0,0 +1,3 @@
+
+oscript .\onescript\corp_translate_feature.os .\\..\\beforeRelease_Nagaeva_RO\DictRuRo.json ".\\..\\beforeRelease_Nagaeva_RU\1_создание организации.feature" ".\\..\\beforeRelease_Nagaeva_RO\1_создание организации translated.feature" 
+pause
